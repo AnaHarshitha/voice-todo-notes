91fba2f0f89a08514a3fd7825b0b1dba233a175d
Harshitha <anaharshitha31@gmail.com>
2025-11-09 13:44:25 +0530

    Rebuild web for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d7cfb8f16b7afb7054a7bef8e4bba756
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "2e19f667dbd341578963a7f4f80aa662",
+"flutter_bootstrap.js": "d857377275b7d549575cd73b016dffa6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
